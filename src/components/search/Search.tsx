1bbd2bd2098ddc5d760bf41b781575b169b97b46
danil87 <kokovin.danil@list.ru>
2024-03-03 18:47:35 +0300

    create new input search

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,8 +1,26 @@
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import style from './Search.module.css';
+
 function Search() {
   return (
-    <>
-      <input type="text" />
-    </>
+    <div className={style.Search}>
+      <Paper
+        component="form"
+        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 500 }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Введите название книги"
+          inputProps={{ 'aria-label': 'search google maps' }}
+        />
+        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+    </div>
   );
 }
 
