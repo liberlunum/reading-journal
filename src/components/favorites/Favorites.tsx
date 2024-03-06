c17df3721688faa6ac5627d210f169ba34a03b96
liberlunum <dghezeriiti@gmail.com>
2024-03-06 20:25:26 +0300

    add request

--- src/components/favorites/Favorites.tsx
+++ src/components/favorites/Favorites.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Favorites.module.css';
 import Card from '@mui/material/Card';
 import { CardMedia } from '@mui/material';
@@ -7,12 +7,24 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import { book } from './favoritesBook.mock';
 
 function Favorites() {
+  const [books, setBooks] = useState<any[]>([]);
+  const bookIds = ['works/OL548432W', 'works/OL547432W', 'works/OL548532W'];
+
+  useEffect(() => {
+    const promises = bookIds.map(bookId =>
+      fetch(` https://openlibrary.org/${bookId}.json`).then(response =>
+        response.json()
+      )
+    );
+    Promise.all(promises).then(response => setBooks(response));
+  }, []);
+
   return (
     <div className={styles.container}>
-      {book.map(book => {
+      {books.map(book => {
         return (
           <Card
-            key={book.id}
+            key={book.key}
             sx={{
               width: 150,
               height: 230,
@@ -37,7 +49,7 @@ function Favorites() {
             </IconButton>
             <CardMedia
               component="img"
-              image={book.cover}
+              image={`https://covers.openlibrary.org/b/id/${book.covers?.[0]}-L.jpg`}
               alt="обложка"
               sx={{ width: '100%', height: '100%' }}
             />
