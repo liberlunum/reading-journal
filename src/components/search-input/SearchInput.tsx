289fd5621c563d28278996425b6494bb64bf33f8
danil87 <kokovin.danil@list.ru>
2024-03-08 15:42:33 +0300

    create suggest

--- src/components/search-input/SearchInput.tsx
+++ src/components/search-input/SearchInput.tsx
@@ -3,19 +3,32 @@ import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import style from './SearchInput.module.css';
-import { FormEvent, useEffect, useState } from 'react';
+import { FormEvent, useCallback, useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { Divider } from '@mui/material';
+import { ClickAwayListener, Divider, Popper } from '@mui/material';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import FilterMenu from '../filter-menu/FilterMenu';
+import { Box } from '@mui/system';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
+import { useDispatch } from 'react-redux';
+import { fetchBooksForSuggest } from '../../state/action-creators/books';
+import BookPopper from '../book-popper/BookPopper';
 
 function SearchInput() {
+  const { books, loading } = useTypedSelector(state => state.booksForSuggest);
+  const dispatch: any = useDispatch();
+  const [timeout, setTime] = useState<NodeJS.Timeout | null>(null);
   const [searchString, setSearchString] = useState('');
   const [searchParams, setSearchParams] = useSearchParams();
+  const [popperEl, setPopperEl] = useState<null | HTMLElement>(null);
   const [filterIconEl, setFilterIconEl] = useState<null | HTMLElement>(null);
 
   const search = (event: MouseEvent | FormEvent) => {
     event.preventDefault();
+
+    clearTime();
+    setPopperEl(null);
+
     searchParams.set('q', searchString);
     searchParams.set('page', '1');
     setSearchParams(searchParams);
@@ -25,45 +38,87 @@ function SearchInput() {
     setFilterIconEl(event.currentTarget);
   };
 
+  const openPopper = useCallback(
+    () => !!(books && (books.length || loading) && popperEl),
+    [books, loading, popperEl]
+  );
+
+  const clearTime = () => {
+    if (timeout) clearTimeout(timeout);
+  };
+
   useEffect(() => {
     setSearchString(searchParams.get('q') || '');
   }, [searchParams]);
+
+  useEffect(() => {
+    clearTime();
+
+    setTime(
+      setTimeout(() => {
+        dispatch(fetchBooksForSuggest(`q=${searchString}`));
+      }, 500)
+    );
+  }, [searchString]);
+
+  useEffect(() => {
+    return () => clearTime();
+  });
 
   return (
     <div className={style.SearchInput}>
       <Paper
         component="form"
         onSubmit={search}
-        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 500 }}
+        sx={{
+          p: '2px 4px',
+          display: 'flex',
+          alignItems: 'center',
+          width: 500,
+          position: 'relative',
+        }}
       >
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="Поиск"
-          value={searchString}
-          onChange={e => setSearchString(e.currentTarget.value)}
-        />
-        <IconButton
-          onClick={openFilterMenu}
-          sx={{ p: '10px' }}
-          aria-label="directions"
-        >
-          <FilterAltIcon />
-        </IconButton>
-        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-        <IconButton
-          onClick={search}
-          type="button"
-          sx={{ p: '10px' }}
-          aria-label="search"
-        >
-          <SearchIcon />
-        </IconButton>
-        <FilterMenu
-          filterIconEl={filterIconEl}
-          handleClose={() => {
-            setFilterIconEl(null);
-          }}
-        />
+        <ClickAwayListener onClickAway={() => setPopperEl(null)}>
+          <Box sx={{ display: 'flex', minWidth: '100%' }}>
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Search"
+              value={searchString}
+              onChange={event => {
+                setSearchString(event.currentTarget.value);
+                if (!popperEl) setPopperEl(event.currentTarget);
+              }}
+            />
+            <IconButton
+              onClick={openFilterMenu}
+              sx={{ p: '10px' }}
+              aria-label="directions"
+            >
+              <FilterAltIcon />
+            </IconButton>
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton
+              onClick={search}
+              type="button"
+              sx={{ p: '10px' }}
+              aria-label="search"
+            >
+              <SearchIcon />
+            </IconButton>
+            <FilterMenu
+              filterIconEl={filterIconEl}
+              handleClose={() => {
+                setFilterIconEl(null);
+              }}
+            />
+            <BookPopper
+              popperEl={popperEl}
+              openPopper={openPopper}
+              books={books}
+              loading={loading}
+            />
+          </Box>
+        </ClickAwayListener>
       </Paper>
     </div>
   );
