abef7f151f8426f30c32156ddfeca8f1aaab57a3
liberlunum <dghezeriiti@gmail.com>
2024-03-08 15:49:54 +0300

    work on favorites

--- src/components/favorites/Favorites.tsx
+++ src/components/favorites/Favorites.tsx
@@ -1,62 +1,68 @@
 import React, { useEffect, useState } from 'react';
 import styles from './Favorites.module.css';
 import Card from '@mui/material/Card';
-import { CardMedia } from '@mui/material';
+import { Box, CardMedia, CircularProgress } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import { book } from './favoritesBook.mock';
+import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
+import customStyles from './customStyles';
 
 function Favorites() {
   const [books, setBooks] = useState<any[]>([]);
-  const bookIds = ['works/OL548432W', 'works/OL547432W', 'works/OL548532W'];
+  const [loading, setLoading] = useState<boolean>(true);
+  const bookIds = ['works/OL863142W', 'works/OL863642W', 'works/OL860542W'];
+
+  const deleteBook = (key: string) => {
+    setBooks(prevState => prevState.filter(book => book.key !== key));
+  };
 
   useEffect(() => {
     const promises = bookIds.map(bookId =>
-      fetch(` https://openlibrary.org/${bookId}.json`).then(response =>
-        response.json()
-      )
+      fetch(` https://openlibrary.org/${bookId}.json`)
+        .then(response => response.json())
+        .catch()
     );
-    Promise.all(promises).then(response => setBooks(response));
+    Promise.all(promises)
+      .then(response => setBooks(response))
+      .catch()
+      .finally(() => setLoading(false));
   }, []);
 
   return (
     <div className={styles.container}>
+      {loading && (
+        <Box sx={customStyles.box}>
+          <CircularProgress variant="indeterminate" sx={customStyles.loader} />
+        </Box>
+      )}
+
       {books.map(book => {
         return (
-          <Card
-            key={book.key}
-            sx={{
-              width: 150,
-              height: 230,
-              position: 'relative',
-              margin: 2,
-            }}
-          >
+          <Card key={book.key} sx={customStyles.card}>
+            {!book.covers?.[0] && (
+              <Typography sx={customStyles.typography}>
+                "{book.title.slice(0, 22) + '..'}"
+              </Typography>
+            )}
             <IconButton
-              sx={{
-                position: 'absolute',
-                top: -5,
-                right: -5,
-                zIndex: 1,
-                color: '#C41E3A',
-                opacity: 0.7,
-                '&:hover': {
-                  opacity: 1,
-                },
-              }}
+              onClick={() => deleteBook(book.key)}
+              sx={customStyles.iconButton}
             >
               <FavoriteIcon />
             </IconButton>
-            <CardMedia
-              component="img"
-              image={
-                book.covers?.[0]
-                  ? `https://covers.openlibrary.org/b/id/${book.covers?.[0]}-L.jpg`
-                  : 'https://openlibrary.org/images/icons/avatar_book-sm.png'
-              }
-              alt="обложка"
-              sx={{ width: '100%', height: '100%' }}
-            />
+            <Link to={book.key}>
+              <CardMedia
+                component="img"
+                image={
+                  book.covers?.[0]
+                    ? `https://covers.openlibrary.org/b/id/${book.covers?.[0]}-M.jpg`
+                    : 'https://openlibrary.org/images/icons/avatar_book-sm.png'
+                }
+                alt="обложка"
+                sx={customStyles.cardMedia}
+              />
+            </Link>
           </Card>
         );
       })}
---
+++ src/components/favorites/customStyles.ts
@@ -0,0 +1,39 @@
+const customStyles = {
+  box: {
+    width: '100%',
+  },
+  loader: {
+    color: '#44003e',
+  },
+  card: {
+    width: 150,
+    height: 230,
+    position: 'relative',
+    margin: 2,
+  },
+  typography: {
+    color: '#44003e',
+    position: 'absolute',
+    width: '100%',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  iconButton: {
+    position: 'absolute',
+    top: -5,
+    right: -5,
+    zIndex: 1,
+    color: '#C41E3A',
+    opacity: 0.7,
+    '&:hover': {
+      opacity: 1,
+    },
+  },
+  cardMedia: {
+    width: '100%',
+    height: '100%',
+  },
+};
+
+export default customStyles;
