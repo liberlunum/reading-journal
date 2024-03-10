c8103c21f7ad0fc15fc952a9f1327574a6d0ad0b
liberlunum <dghezeriiti@gmail.com>
2024-03-10 17:56:01 +0300

    add favorites empty state

--- src/components/favorites/Favorites.tsx
+++ src/components/favorites/Favorites.tsx
@@ -1,72 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import styles from './Favorites.module.css';
-import Card from '@mui/material/Card';
-import { Box, CardMedia, CircularProgress } from '@mui/material';
-import IconButton from '@mui/material/IconButton';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import Typography from '@mui/material/Typography';
-import { Link } from 'react-router-dom';
-import customStyles from './customStyles';
+import EmptyFavorites from './EmptyFavorites';
+import ShowFavorites from './ShowFavorites';
 
 function Favorites() {
-  const [books, setBooks] = useState<any[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const bookIds = ['works/OL863142W', 'works/OL863642W', 'works/OL860542W'];
-
-  const deleteBook = (key: string) => {
-    setBooks(prevState => prevState.filter(book => book.key !== key));
-  };
-
-  useEffect(() => {
-    const promises = bookIds.map(bookId =>
-      fetch(` https://openlibrary.org/${bookId}.json`)
-        .then(response => response.json())
-        .catch()
-    );
-    Promise.all(promises)
-      .then(response => setBooks(response))
-      .catch()
-      .finally(() => setLoading(false));
-  }, []);
-
   return (
-    <div className={styles.container}>
-      {loading && (
-        <Box sx={customStyles.box}>
-          <CircularProgress variant="indeterminate" sx={customStyles.loader} />
-        </Box>
-      )}
-
-      {books.map(book => {
-        return (
-          <Card key={book.key} sx={customStyles.card}>
-            {!book.covers?.[0] && (
-              <Typography sx={customStyles.typography}>
-                "{book.title.slice(0, 22) + '..'}"
-              </Typography>
-            )}
-            <IconButton
-              onClick={() => deleteBook(book.key)}
-              sx={customStyles.iconButton}
-            >
-              <FavoriteIcon />
-            </IconButton>
-            <Link to={book.key}>
-              <CardMedia
-                component="img"
-                image={
-                  book.covers?.[0]
-                    ? `https://covers.openlibrary.org/b/id/${book.covers?.[0]}-M.jpg`
-                    : 'https://openlibrary.org/images/icons/avatar_book-sm.png'
-                }
-                alt="обложка"
-                sx={customStyles.cardMedia}
-              />
-            </Link>
-          </Card>
-        );
-      })}
-    </div>
+    <>
+      <EmptyFavorites></EmptyFavorites>
+      {/*  <ShowFavorites></ShowFavorites>*/}
+    </>
   );
 }
 
