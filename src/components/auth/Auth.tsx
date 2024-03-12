3b2665078e1d4ad318b945e7f48d6dc70ebf6538
Artyom Rulinskii <r.artyom2012@yandex.ru>
2024-03-12 13:13:43 +0300

    uniqueness validation bug fix

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -28,6 +28,7 @@ export default function Auth({
       localStorage.setItem('CurrentUser', JSON.stringify(userData));
       dispatch(Login(userData));
       navigate('/');
+      console.log('userData', userData);
     },
     [dispatch, navigate]
   );
@@ -53,7 +54,7 @@ export default function Auth({
     setRegisterModal({ show: true, login: user.login });
   };
   const checkUniqueness = (auth: AuthPrompt) => {
-    UsersRef.current.findIndex(el => el.login === auth.login) > 0
+    UsersRef.current.findIndex(el => el.login === auth.login) >= 0
       ? alert('Login is not unique')
       : uploadToLocalStorage({
           login: auth.login,
